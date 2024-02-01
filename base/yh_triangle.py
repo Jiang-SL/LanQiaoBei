027c5515abaea1d29e0abab46998458061b8141b
jiangsiliang <isjiangsl@foxmail.com>
2024-02-01 14:21:47 +0800

    杨辉三角形

--- base/yh_triangle.py
+++ base/yh_triangle.py
@@ -5,4 +5,26 @@
 # @brief   : 输出杨辉三角，三角形中的每个数字等于它两肩上的数字相加。
 # @Software: PyCharm
 
-def
+def yh_triangle(n):
+    triangle = [[0] * n for i in range(n)]  # 初始化n*n的0矩阵
+
+    for i in range(n):
+        for j in range(i+1):
+            if j == 0 and j == i:
+                triangle[i][j] = 1  # 最边边的元素都为1
+            else:
+                # 两肩之和等于当前的元素
+                triangle[i][j] = triangle[i - 1][j - 1] + triangle[i - 1][j]
+    else:
+        for i in range(n):  # 遍历每一行
+            for num in triangle[i]:  # 遍历每一行的各个元素
+                if num==0:  # 不输出多余的0
+                    break
+                else:
+                    print(num,end=' ')
+            print()  # 换行
+
+
+if __name__ == "__main__":
+    n = int(input())  # 输出n的值，1<=n<=34
+    yh_triangle(n)  # 输出杨辉三角
