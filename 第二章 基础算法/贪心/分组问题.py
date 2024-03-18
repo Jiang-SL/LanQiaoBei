2d00907976e93ce02d0f2787ec59076efbc730cd
Jiang-SL <isjiangsl@foxmail.com>
2024-03-18 20:27:21 +0800

    贪心算法的分组问题（未完成）

---
+++ 第二章 基础算法/贪心/分组问题.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2024/3/5 10:08
+# @Author  : isjiangsl@foxmail.com
+# @File    : 分组问题.py
+# @Brief   :
+# @Software: PyCharm
+
+def main():
+    w = int(input())  # 最大价格和上限
+
+
+
+if __name__ == "__main__":
+    main()
